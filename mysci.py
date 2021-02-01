1de245716f8582ec6b91f840a29b65c50e17fd03
David <dsalis@ucar.edu>
2021-02-01 13:21:04 -0700

    Refactoring data parsing code

--- mysci.py
+++ mysci.py
@@ -2,60 +2,50 @@
 # comment
 
 data = []  #list
-data2 = {'date':[], 'time':[], 'tempout':[]} #initialize to lists with names
-
+data = {}  #dictionary
+#you can index into lists.. and strings
+# column names and column indeces to read
 columns = {'date':0, 'time':1, 'tempout':2} #dictionary
 
-types = {'tempout': float}
+# Data types for each column ( only if non-string )
+types = {'tempout': float} # convert to a float ( float is a function ptr )
+
+#data = {'date':[], 'time':[], 'tempout':[]} #initialize to lists with names # hardcode deprecated
+#time = data['time']
 for column in columns:
-    #data[column] = []  # initialize to be a list so as to be able to append it.
+    data[column] = []  # add a new key.  initialize the list so as to be able to append it.
     print (column)
+
 # use [] tp get to an item no matter what it is,
 
 filename = "data/wxobs20170821.txt"
 
-#datafile = open (filename,"r")
-#print (datafile.readline())
-
-#data = datafile.read() # read the whole file in 
-#datafile.close()
-
-# with - closes file, and 4 spaces needs to be consistent
+for column in columns: # loop through each of the keys in our dictionary 
+    data[column] = [] # data[date], data[time]..
 
 # a "context manager" # setting it to the name datafile.
 
-with open(filename,"r") as datafile: 
+with open(filename,"r") as datafile:  # closes the file too 
     #data = datafile.read()  
     for i in range(3): 
         datafile.readline()
     # read and parse the rest of the file
     for line in datafile:
         split_line = line.split()  # blank value means split on whitespace, default.
-        for column in columns:
-            i = columns[column]
-            t = types.get(column, str)
+        # instead of the below we do the following for loop
+        #data['date'].append(split_line[0])
+        #data['time'].append(split_line[1])
+        #data['tempout'].append(float(split_line[2]))
+        for column in columns:   # make our new data dictionary
+            i = columns[column]  # i shorthand for index  i will be 0, 1 or 2
+            t = types.get(column,str)
+            print(t)
             value = t(split_line[i])
-            data.append(value) 
-        # datum is now a list of the split up string
-        # data.append(datum) # adds another record to the data arrayist 
-            data2['date'].append(split_line[0])
+            data[column].append(value) 
 
 # negative indexes start at the end instead of the beginning
 
-
-#print(data[8])
-#print(data[8][4])
-#print(data[8][4][0])
-#print(data[8][::2]) # no start or end but always every other
-#print(data[1:3])
-#print(data[-1]) # last row
-#for datum in data[:3]:  #[:4:2] - start at 0, do 4 by 2s
-#   print(datum)
-
-#print(type(data))
-
-
-
+#print (data['tempout'])
 
 
 
