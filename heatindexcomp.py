4bca24823ff13f5d40ced39f5b6f35b0ebf0ca05
David <dsalis@ucar.edu>
2021-02-01 10:55:51 -0700

    same again

--- heatindexcomp.py
+++ heatindexcomp.py
@@ -1,7 +1,10 @@
-#!/usr/bin/python
-# comment
+#!/usr/bin/python # comment
 
 from readdata import read_data
+from printing import print_comparison
+from computation import compute_windchill
+
+data = read_cata(columns, data={})
 
 columns = { data:1 }
 def compute_windchill(t,v):
@@ -9,7 +12,9 @@ def compute_windchill(t,v):
    b = 0.62
    c = 35
    d = 0.42
-   return
+
+   wci = a
+   return a
 
 
 
