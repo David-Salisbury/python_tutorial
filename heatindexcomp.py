fdf7881c19ba36b82251a55273b58e0209c41eea
David <dsalis@ucar.edu>
2021-02-10 11:28:20 -0700

    updating new heat index script

--- heatindexcomp.py
+++ heatindexcomp.py
@@ -6,10 +6,10 @@ data = {}  #dictionary
 #you can index into lists.. and strings
 # column names and column indeces to read
 columns = {'date':0, 'time':1, 'tempout':2} #dictionary
-columns = {'date':0, 'time':1, 'tempout':2, 'windspeed':7, 'windchill':12} #dictionary - position in data list
+columns = {'date':0, 'time':1, 'tempout':2, 'humout':5, 'heatindex' : 13} #dictionary - position in data list
 
 # Data types for each column ( only if non-string )
-types = {'tempout': float, 'windspeed':float, 'windchill':float} # convert to a float ( float is a function ptr )
+types = {'tempout': float, 'humout' : float, 'heatindex' : float} # convert to a float ( float is a function ptr )
 
 #data = {'date':[], 'time':[], 'tempout':[]} #initialize to lists with names # hardcode deprecated
 #time = data['time']
@@ -46,39 +46,49 @@ with open(filename,"r") as datafile:  # closes the file too
 
 # negative indexes start at the end instead of the beginning
 
-# Compute the wind chill temperature
+# Compute heat index 
 
-def compute_windchill(temp, windspeed):
-    a = 35.74
-    b = 0.6215
-    c = 35.75
-    d = 0.4275
+def compute_heatindex(temp, humidity):
+    a = -42.379
+    b = 2.04901523
+    c = 10.14333127
+    d = 0.22475541
+    e = 0.00683783
+    f = 0.05481717
+    g = 0.00122874
+    h = 0.00085282
+    i = 0.00000199
 
-    v16 = windspeed ** 0.16
-    windchill = a + ( b * temp) - ( c * v16) + ( d * temp * v16 )
+    rh = humidity / 100
 
-    return windchill
+    heatindex = ( a + (b * temp ) + ( c * rh ) +
+                 ( d * temp * rh ) +
+                 ( e * temp ** 2 ) +
+                 ( f * rh ** 2 ) + ( g * temp ** 2 * rh ) +
+                 ( h * temp * rh ** 2 ) +
+                 ( i * temp ** 2 * rh ** 2 ))
 
+    return heatindex
 
 
 
 #print (data['tempout'])
 
-windchill = []
-for temp, windspeed in zip(data['tempout'], data['windspeed']):
-    windchill.append(compute_windchill(temp,windspeed))
+heatindex = []
+for temp, humout in zip(data['tempout'], data['humout']):
+    heatindex.append(compute_heatindex(temp,humout))
 
 # for wc_data, wc_comp in zip (windchill, data['windchill']):
 #     print(f'{wc_data:.5f} {wc_comp:.5f} {wc_data - wc_comp:.5f}')
 
 print ('               ORIGINAL  COMPUTED')
-print (' DATE    TIME WINDCHILL  WINDCHILL DIFFERENCE')
+print (' DATE    TIME HEATINDEX HEATINDEX  DIFFERENCE')
 print ('------- ----- ---------- --------- ----------')
-zip_data = zip(data['date'], data['time'], data['windchill'], windchill )
-for date, time , wc_orig, wc_comp in zip_data:
-    wc_diff = wc_orig - wc_comp
+zip_data = zip(data['date'], data['time'], data['heatindex'], heatindex )
+for date, time , hi_orig, hi_comp in zip_data:
+    hi_diff = hi_orig - hi_comp
     #> right justified
-    print (f'{date} {time:>6} {wc_orig:9.6f} {wc_comp:9.6f} {wc_diff:10.6f}')
+    print (f'{date} {time:>6} {hi_orig:9.6f} {hi_comp:9.6f} {hi_diff:10.6f}')
 
 
 
