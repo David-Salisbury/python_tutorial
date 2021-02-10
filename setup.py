b89eee4d87d5a1ac0d2e2f91303191de3858a043
David <dsalis@ucar.edu>
2021-02-10 13:47:02 -0700

    adding setuip.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description = "a sample package",
+    author="Xdev",
+    author_email="none",
+    packages=["mysci"],
+    install_requires=[] 
+    )
